fa21fcd3d14c8cbc2efe4a00305f56f869941905
Bradley Wangia <bwangia@50onred.com>
2014-08-11 13:59:09 -0400

    cleanup

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,10 @@
 chef-server-rds cookbook README
 ==========================
 
+v1.0.1
+------
+- Cleanup changelog and release versions
+
 v1.0.0
 ------
 - Initial release 
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email  'bradley.wangia@gmail.com'
 license           'Apache 2.0'
 description       'Installs and configures Chef Server backed by Amazon RDS Postgresql'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           '1.0.0'
+version           '1.0.1'
 
 supports 'ubuntu'
 
