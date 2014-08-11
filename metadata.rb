2f5c83f43c93cd197a54f05b847279469446c866
Bradley Wangia <bwangia@50onred.com>
2014-08-11 13:39:04 -0400

    update maintainer

--- metadata.rb
+++ metadata.rb
@@ -1,13 +1,11 @@
-name              'chef-server'
-maintainer        'Opscode, Inc.'
-maintainer_email  'cookbooks@opscode.com'
+name              'chef-server-rds'
+maintainer        'Bradley Wangia'
+maintainer_email  'bradley.wangia@gmail.com'
 license           'Apache 2.0'
-description       'Installs and configures Chef Server'
+description       'Installs and configures Chef Server backed by Amazon RDS Postgresql'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           '2.1.7'
+version           '1.0.0'
 
-supports 'centos'
-supports 'amazon'
 supports 'ubuntu'
 
 # depends 'git'
